272692a554dc6a3816d8b570ade3303da2a6cc0b
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-30 10:32:08 -0300

    Setting App and installing packages

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set ("views", path.resolve (__dirname, "views"));
+
+app.listen(app.get('port'), ()=> console.log('Listening on http://localhost:' + app.get('port')));
+
+app.use(express.static(path.resolve(__dirname, "../public")));
