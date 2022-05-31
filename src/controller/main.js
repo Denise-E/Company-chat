578248cc319e3f6a8cb0a37d13e7ac2c3a3c9bfb
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-05-31 13:00:27 -0300

    Creating login and register GET routes

--- src/controller/main.js
+++ src/controller/main.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    index: (req, res) => res.sendFile(path.resolve(__dirname, '../views/index.html'))
+    index: (req, res) => res.sendFile(path.resolve(__dirname, '../views/index.html')),
+    login: (req, res) => res.sendFile(path.resolve(__dirname, '../views/login.html')),
+    register: (req, res) => res.sendFile(path.resolve(__dirname, '../views/register.html')),
 }
--- src/router/main.js
+++ src/router/main.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const main = require('../controller/main.js')
 
 router.get('/', main.index);
+router.get('/login', main.login);
+router.get('/register', main.register);
 
 module.exports = router;
